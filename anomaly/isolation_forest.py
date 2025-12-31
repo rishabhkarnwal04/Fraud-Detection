56e2c390be3d400b2c358f251a181d1bde5450dc
Rishabh Karnwal <134758111+rishabhkarnwal04@users.noreply.github.com>
2025-12-31 21:26:56 +0530

    Update isolation_forest.py

--- anomaly/isolation_forest.py
+++ anomaly/isolation_forest.py
@@ -1,6 +1,5 @@
 from sklearn.ensemble import IsolationForest
 
-
 def detect_anomalies(X):
-iso = IsolationForest(contamination=0.02, random_state=42)
-return (iso.fit_predict(X)==-1).astype(int)
+    iso = IsolationForest(contamination=0.02, random_state=42)
+    return (iso.fit_predict(X) == -1).astype(int)
