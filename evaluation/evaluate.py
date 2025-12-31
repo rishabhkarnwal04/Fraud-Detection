3f82a78acaea7973d4c306e8f71d58cc8e63ea31
Rishabh Karnwal <134758111+rishabhkarnwal04@users.noreply.github.com>
2025-12-31 21:25:40 +0530

    Update evaluate.py

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,8 +1,12 @@
 from sklearn.metrics import classification_report, roc_auc_score
 
-
 def evaluate(model, X_test, y_test):
-preds = model.predict(X_test)
-probas = model.predict_proba(X_test)[:,1]
-print(classification_report(y_test, preds))
-print('ROC-AUC:', roc_auc_score(y_test, probas))
+    # Predict labels
+    preds = model.predict(X_test)
+
+    # Predict probabilities
+    probas = model.predict_proba(X_test)[:, 1]
+
+    # Print classification metrics
+    print(classification_report(y_test, preds))
+    print('ROC-AUC:', roc_auc_score(y_test, probas))
